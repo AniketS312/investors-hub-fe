409262a41d21356d1e91189c33e091bd770cf5e7
AniketS312 <aniketsharma312@gmail.com>
2021-07-21 11:03:15 -0600

    adjusted NAVIGATION img size and padding

--- src/components/Ideas/Ideas.tsx
+++ src/components/Ideas/Ideas.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import Navigation from "../layout/Navigation";
 
 function Ideas() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <h1>Entrepreneurs</h1>;
+    </>
+  );
 }
 
 export default Ideas;
