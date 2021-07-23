33aa17fdb473bf09204db33732fbf45089a35335
AniketS312 <aniketsharma312@gmail.com>
2021-07-22 18:13:41 -0600

    made the fading effect better? lets discuss later

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,70 +1,70 @@
-import { useEffect, useRef } from 'react'
-import Navigation from '../layout/Navigation'
-import EnterName from '../layout/EnterName'
-import Blobs from '../layout/Blobs'
-import { React } from 'react'
+import { useEffect, useRef } from "react";
+import Navigation from "../layout/Navigation";
+import EnterName from "../layout/EnterName";
+import Blobs from "../layout/Blobs";
+import { React } from "react";
 
 const Welcome = () => {
-	localStorage.clear();
-	const containerRef = useRef(null);
-	const inputNameContainerRef = useRef(null);
+  localStorage.clear();
+  const containerRef = useRef(null);
+  const inputNameContainerRef = useRef(null);
 
-	useEffect(() => {
-		inputNameContainerRef.current.style.opacity = '0';
-		containerRef.current.style.opacity = '0';
-	 	unFade(containerRef.current);
-		setTimeout(() => {fade(containerRef.current)}, 1800);
-		setTimeout(() => {unFade(inputNameContainerRef.current)}, 3000);
-	})
+  useEffect(() => {
+    inputNameContainerRef.current.style.opacity = "0";
+    containerRef.current.style.opacity = "0";
+    unFade(containerRef.current);
+    setTimeout(() => {
+      fade(containerRef.current);
+    }, 1300);
+    setTimeout(() => {
+      unFade(inputNameContainerRef.current);
+    }, 1500);
+  });
 
-   	function fade(element) {
-    var initialOpacity = 1; 
+  function fade(element) {
+    var initialOpacity = 1;
     var timer = setInterval(function () {
-        if (initialOpacity <= 0.05){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = initialOpacity;
-        element.style.filter = 'alpha(opacity=' + initialOpacity * 100 + ")";
-        initialOpacity -= initialOpacity * 0.09;
+      if (initialOpacity <= 0.05) {
+        clearInterval(timer);
+        element.style.display = "none";
+      }
+      element.style.opacity = initialOpacity;
+      element.style.filter = "alpha(opacity=" + initialOpacity * 100 + ")";
+      initialOpacity -= initialOpacity * 0.5;
     }, 50);
-	}
+  }
 
-	function unFade(element) {
-		var initialOpacity = 0.1;  
-		var timer = setInterval(function () {
-				if (initialOpacity >= 1){
-						clearInterval(timer);
-				}
-				element.style.opacity = initialOpacity;
-				element.style.filter = 'alpha(opacity=' + initialOpacity * 100 + ")";
-				initialOpacity += initialOpacity * 0.09;
-		}, 50);
-   }
-   
-	return (
-		<div className='welcome'>
-			<Blobs />
-			<Navigation />
+  function unFade(element) {
+    var initialOpacity = 0.1;
+    var timer = setInterval(function () {
+      if (initialOpacity >= 1) {
+        clearInterval(timer);
+      }
+      element.style.opacity = initialOpacity;
+      element.style.filter = "alpha(opacity=" + initialOpacity * 100 + ")";
+      initialOpacity += initialOpacity * 0.15;
+    }, 50);
+  }
 
-			<div className='container' ref={containerRef}>
-				<div className='information__container'>
-					<h2 className='enter-name'>Welcome to Investors Hub!</h2>
-				</div>
-			</div>
+  return (
+    <div className="welcome">
+      <Blobs />
+      <Navigation />
 
-			<div className='container' ref={inputNameContainerRef}>
-				<div className='information__container'>
-					<h2 className='enter-name'>Enter your name</h2>
-            <EnterName />
-				</div>
-			</div>
+      <div className="container" ref={containerRef}>
+        <div className="information__container">
+          <h2 className="enter-name">Welcome to Investors Hub!</h2>
+        </div>
+      </div>
 
-		</div>
-	)
+      <div className="container" ref={inputNameContainerRef}>
+        <div className="information__container">
+          <h2 className="enter-name">Enter your name</h2>
+          <EnterName />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-}
-
-export default Welcome
+export default Welcome;
