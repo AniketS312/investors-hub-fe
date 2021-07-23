f6d115600487983692fbdb361a9c12453066d32f
AniketS312 <aniketsharma312@gmail.com>
2021-07-22 21:18:28 -0600

    made page for idea box, still trying to map, could not get border radius on search border

--- src/components/Ideas/Ideas.tsx
+++ src/components/Ideas/Ideas.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import Navigation from "../layout/Navigation";
+import IdeaBox from "../layout/IdeaBox";
 
 function Ideas() {
+  function onclick(event: any) {
+    event.preventDefault;
+    document.getElementById("search")?.setAttribute("value", " ");
+  }
   return (
     <div className="ideas">
       <Navigation />
       <div className="container">
         <div className="container__investors">
           <h1>Entrepreneurs</h1>
-          <p>Talk to an idealist</p>
+          <h2>Talk to an idealist</h2>
+          <div className="ideas-container">
+            <IdeaBox />
+          </div>
         </div>
         <div className="container__search">
-          <input type="search" name="Search" id="search" value="Search" />
+          <input
+            type="search"
+            onClick={(event) => event.preventDefault}
+            name="Search"
+            id="search"
+            placeholder="Search"
+          />
         </div>
       </div>
     </div>
---
+++ src/components/layout/IdeaBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function IdeaBox() {
+  return (
+    <div className="idea">
+      <h3>Crypto Payment System</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis est
+        interdum felis urna, aliquet integer habitasse eu.
+      </p>
+      <button>See More</button>
+    </div>
+  );
+}
+
+export default IdeaBox;
